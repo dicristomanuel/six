e9e5d2912c6f0435e677b9af8d184430c29f0e74
dicristomanuel <manuel.dicristo@raise.com>
2016-08-22 14:16:43 -0500

    Initial Commit

---
+++ six/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  six
+//
+//  Created by Manuel Di Cristo on 8/22/16.
+//  Copyright © 2016 six. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
