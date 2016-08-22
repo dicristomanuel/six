636a0a1b42ad26a2a6ced17cdf582a743350e24a
dicristomanuel <manuel.dicristo@raise.com>
2016-08-22 15:14:09 -0500

    [app] build six and quit action

--- six/AppDelegate.swift
+++ six/AppDelegate.swift
@@ -12,22 +12,33 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-
+    let menu = NSMenu()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         if let button = statusItem.button {
             button.image = NSImage(named: "iconsix")
-            button.action = Selector("showMenu:")
+        }
+        
+        menu.addItem(NSMenuItem(title: "Six", action: #selector(AppDelegate.showMenu(_:)), keyEquivalent: "S"))
+        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+        
+        func keyDown(theEvent: NSEvent) {
+            print(theEvent.keyCode)
         }
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        
-    }
+    func applicationWillTerminate(aNotification: NSNotification) {}
     
     func showMenu(sender: NSStatusBarButton) {
         print("Menu is open")
     }
-
+    
+    func quit(send: AnyObject?) {
+        NSApplication.sharedApplication().terminate(nil)
+    }
 
 }
 
