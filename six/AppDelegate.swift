a9dc18ec5533311cbcb77bbe5796f54eab801c53
dicristomanuel <manuel.dicristo@raise.com>
2016-08-22 17:05:30 -0500

    [AppDelegate] fix indentation

--- six/AppDelegate.swift
+++ six/AppDelegate.swift
@@ -11,34 +11,33 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-    let menu = NSMenu()
-    
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        if let button = statusItem.button {
-            button.image = NSImage(named: "iconsix")
-        }
-        
-        menu.addItem(NSMenuItem(title: "Six", action: #selector(AppDelegate.showMenu(_:)), keyEquivalent: "S"))
-        menu.addItem(NSMenuItem.separatorItem())
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit(_:)), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
-        
-        func keyDown(theEvent: NSEvent) {
-            print(theEvent.keyCode)
-        }
-    }
+  let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+  let menu = NSMenu()
 
-    func applicationWillTerminate(aNotification: NSNotification) {}
-    
-    func showMenu(sender: NSStatusBarButton) {
-        print("Menu is open")
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    if let button = statusItem.button {
+      button.image = NSImage(named: "iconsix")
     }
     
-    func quit(send: AnyObject?) {
-        NSApplication.sharedApplication().terminate(nil)
+    menu.addItem(NSMenuItem(title: "Six", action: #selector(AppDelegate.showMenu(_:)), keyEquivalent: "S"))
+    menu.addItem(NSMenuItem.separatorItem())
+    menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit(_:)), keyEquivalent: "q"))
+
+    statusItem.menu = menu
+
+    func keyDown(theEvent: NSEvent) {
+      print(theEvent.keyCode)
     }
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {}
+
+  func showMenu(sender: NSStatusBarButton) {
+    print("Menu is open")
+  }
+
+  func quit(send: AnyObject?) {
+    NSApplication.sharedApplication().terminate(nil)
+  }
 
 }
-
