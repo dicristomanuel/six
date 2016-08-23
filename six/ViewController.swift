29ee2f18c8202f906a0a11a791584ee8c828dedc
dicristomanuel <manuel.dicristo@raise.com>
2016-08-23 11:25:33 -0500

    [ViewController] remove comments

--- six/ViewController.swift
+++ six/ViewController.swift
@@ -1,27 +1,14 @@
-//
-//  ViewController.swift
-//  six
-//
-//  Created by Manuel Di Cristo on 8/22/16.
-//  Copyright © 2016 six. All rights reserved.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+        didSet {}
     }
 
 
 }
-
