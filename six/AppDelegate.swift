749c595fc74f3967dc86d0dc510d394a4b5de155
dicristomanuel <manuel.dicristo@raise.com>
2016-08-24 18:08:05 -0500

    [app] setting up bluetooth

--- six/AppDelegate.swift
+++ six/AppDelegate.swift
@@ -6,13 +6,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
 
   let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
   let menu = NSMenu()
-  var eventMonitor: EventMonitor?
+  var eventMonitor: EventMonitor? = nil
+  var ble: PeripheralWindowController? = nil
 
   func applicationDidFinishLaunching(aNotification: NSNotification) {
     NSUserNotificationCenter.defaultUserNotificationCenter().delegate = self
     setupButton()
     buildMenu()
     setupEventMonitor()
+    startBluetooth()
   }
 
   func applicationWillTerminate(aNotification: NSNotification) {}
@@ -44,4 +46,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
     }
     eventMonitor?.start()
   }
+    
+    func startBluetooth() {
+        ble?.toPrint()
+    }
 }
